675beab1b3579dc04e6b2ad915cfa195e82e41fa
Troy Sellers <tsellers@salesforce.com>
2012-11-23 13:12:26 +1100

    connect with partner wsdl

--- src/main/java/com/force/aus/outboundMessage/AccountOMImpl.java
+++ src/main/java/com/force/aus/outboundMessage/AccountOMImpl.java
@@ -17,6 +17,8 @@ import org.slf4j.LoggerFactory;
 
 import com.force.aus.wsdl.AccountNotification;
 import com.force.aus.wsdl.NotificationPort;
+import com.sforce.soap.partner.PartnerConnection;
+import com.sforce.ws.ConnectorConfig;
 
 @WebService(targetNamespace="http://soap.sforce.com/2005/09/outbound")
 @SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL)
@@ -48,6 +50,8 @@ public class AccountOMImpl implements NotificationPort{
 		logger.info("Enterprise URL ["+enterpriseURL+"]");
 		logger.info("Partner URL ["+partnerURL+"]");
 		
+		process(partnerURL, sessionId);
+		
 		for(AccountNotification an : notificationList) {
 			logger.info("ID of object that has been changed ["+an.getId()+"]");
 		}
@@ -56,6 +60,29 @@ public class AccountOMImpl implements NotificationPort{
 	}
 
 	
-	
+	private void process(String partnerURL, String sessionID) {
+		
+		PartnerConnection connection = null;
+		
+		try {
+			ConnectorConfig config = new ConnectorConfig();
+			config.setSessionId(sessionID);
+			config.setServiceEndpoint(partnerURL);
+			config.setCompression(true);
+			config.setTraceMessage(true);
+			
+			connection = new PartnerConnection(config);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.logout();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 	
 }
