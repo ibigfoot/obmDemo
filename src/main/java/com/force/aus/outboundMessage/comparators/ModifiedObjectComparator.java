f497ca5ca6a666b36c747712522e2a9e760416ec
tsellers <tsellers@salesforce.com>
2013-01-18 16:08:27 +1100

    fix comparator null pointer

--- src/main/java/com/force/aus/outboundMessage/comparators/ModifiedObjectComparator.java
+++ src/main/java/com/force/aus/outboundMessage/comparators/ModifiedObjectComparator.java
@@ -9,8 +9,14 @@ public class ModifiedObjectComparator implements Comparator<ModifiedObject>{
 	@Override
 	public int compare(ModifiedObject o1, ModifiedObject o2) {
 		
-		return o1.getObjectName().toLowerCase().compareTo(o2.getObjectName().toLowerCase());
+		if(o1.getObjectName() != null && o2.getObjectName() != null)	
+			return o1.getObjectName().toLowerCase().compareTo(o2.getObjectName().toLowerCase());
+		if(o1.getObjectName() == null && o2.getObjectName() != null) 
+			return -1;
+		if(o1.getObjectName() != null && o2.getObjectName() == null) 
+			return 1;
 		
+		return 0;
 	}
 
 }
