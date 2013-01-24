2a2384308f8fb95474e89b1e8d3e4a28832756b2
tsellers <tsellers@salesforce.com>
2013-01-24 21:29:50 +1100

    log oauth out

--- src/main/java/com/force/aus/outboundMessage/actions/OAuthCallbackAction.java
+++ src/main/java/com/force/aus/outboundMessage/actions/OAuthCallbackAction.java
@@ -1,12 +1,24 @@
 package com.force.aus.outboundMessage.actions;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
 public class OAuthCallbackAction extends BaseOBMAction {
 
 	@Override
 	public String doExecute() {
 		
 		addActionMessage("We have received a call for the OAuth Callback - need to figure out how to access stuff");
+		HttpServletRequest request = ServletActionContext.getRequest();
 		
+		Enumeration<String> e = request.getAttributeNames();
+		while(e.hasMoreElements()) {
+			String element = e.nextElement();
+			logger.info("Element {} value {}", element, request.getAttribute(element));
+		}
 		return SUCCESS;
 	}
 	
